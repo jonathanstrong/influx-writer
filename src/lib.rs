fa0d5b68fb970d80f0a1eea92fa9005f7e7774b1
Jonathan Strong <jstrong@legis.io>
2017-12-30 00:07:29 -0500

    sets file_logger to return a noop logger if cfg(test)

--- src/lib.rs
+++ src/lib.rs
@@ -44,11 +44,19 @@ pub fn nanos(t: DateTime<Utc>) -> u64 {
     (t.timestamp() as u64) * 1_000_000_000_u64 + (t.timestamp_subsec_nanos() as u64)
 }
 
+#[cfg(not(test))]
 pub fn file_logger(path: &str, level: Severity) -> slog::Logger {
     let mut builder = FileLoggerBuilder::new(path);
     builder.level(level);
     builder.timezone(TimeZone::Utc);
     builder.build().unwrap()
+}
+
+
+#[cfg(test)]
+pub fn file_logger(path: &str, level: Severity) -> slog::Logger {
+    use slog::*;
+    Logger::root(Discard, o!())
 }
 
 pub fn dt_nanos(t: DateTime<Utc>) -> i64 {
